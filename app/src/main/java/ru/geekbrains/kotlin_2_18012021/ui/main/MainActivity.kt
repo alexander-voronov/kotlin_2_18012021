59be7a11875af687068198b258acaf3a95d5ce44
Alexander Voronov <dr.strum@gmail.com>
2021-01-25 17:44:40 +0300

    HW_2_25/01/2021 fix trouble

--- app/src/main/java/ru/geekbrains/kotlin_2_18012021/ui/main/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin_2_18012021/ui/main/MainActivity.kt
@@ -3,23 +3,27 @@ package ru.geekbrains.kotlin_2_18012021.ui.main
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProviders
 import ru.geekbrains.kotlin_2_18012021.R
-import java.util.Observer
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ru.geekbrains.kotlin_2_18012021.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
     lateinit var adapter: MainAdapter
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        viewModel = ViewModelProviders.of( this ). get (MainViewModel:: class . java )
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         adapter = MainAdapter()
-        mainRecycler.adapter = adapter
-        viewModel.viewState().observe( this , Observer<MainViewState> { t ->
+        binding.mainRecycler.adapter = adapter
+        viewModel.viewState().observe(this, Observer<MainViewState> { t ->
             t?.let { adapter.notes = it.notes }
         })
     }
